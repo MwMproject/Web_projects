7fbf4b9a544e16ba0cff30d0690a61def4faf0e7
Manu <mweiss1377@gmail.com>
2026-03-08 10:13:08 +0100

    modif section services

--- Web/NP_autoecole_web/assets/script/script.js
+++ Web/NP_autoecole_web/assets/script/script.js
@@ -22,3 +22,27 @@ setInterval(() => {
 
   track.style.transform = `translateX(${position}px)`;
 }, 3000);
+
+// TOGGLE CARD
+const buttons = document.querySelectorAll(".toggle-card");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const card = button.closest(".card");
+
+    document.querySelectorAll(".card").forEach((c) => {
+      if (c !== card) {
+        c.classList.remove("open");
+
+        const otherButton = c.querySelector(".toggle-card");
+        if (otherButton) otherButton.textContent = "Découvrir";
+      }
+    });
+
+    card.classList.toggle("open");
+
+    button.textContent = card.classList.contains("open")
+      ? "Réduire"
+      : "Découvrir";
+  });
+});
